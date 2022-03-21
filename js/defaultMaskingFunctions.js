c31bab5d253bcf81dc79cefef07686115d13726d
Chris Harden <hawkeyeul@gmail.com>
2022-03-21 08:35:30 -0500

    Validation And Masking WIP 2

--- js/defaultMaskingFunctions.js
+++ js/defaultMaskingFunctions.js
@@ -1,14 +1,16 @@
-RequestDefaultMaskingFunction = (requestedMakingFunction) => {
-  defaultMakFunctions = {
-    date: (domElement) => {
-      const  elementValue = domElement.value;
-      const mm = elementValue.slice(0,2);
-      const dd = elementValue.slice(3, 5);
-      const yyy = elementValue.split(6,10);
-      
-      domElement.value = `${mm}/${dd}/${yyyy}`;
-    },
-  };
+defaultMakFunctions = {
+  date: (domElement) => {
+    const  elementValue = domElement.value;
+    const mm = elementValue.slice(0,2);
+    const dd = elementValue.slice(3, 5);
+    const yyy = elementValue.split(6,10);
+    
+    domElement.value = `${mm}/${dd}/${yyyy}`;
+  },
+};
+
+GetMaskFunction = (requestedMakingFunction) => {
+
 
   maskingFunction = defaultMakFunctions[requestedMakingFunction];
 
